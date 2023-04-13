a3237b248d35d01582c8ef5f05d902deb3f774e2
Bryskov-AG <santei31334@gmail.com>
2023-04-13 15:11:28 +0200

    Push

---
+++ Урок1.py
@@ -0,0 +1,33 @@
+# import math
+
+# n = int(input())
+# m = int(input())
+
+# day = m/n
+# print(math.ceil(day))
+# ----------------------------
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# print((a//2+a%2)+(b//2+b%2)+(c//2+c%2))
+
+# i = int(input())
+# j = int(input())
+
+# if i == j:
+#     print('недостаточно данных')  
+# else:
+#     print(i+j-1)  
+
+# a = int(input())
+
+# if a%4 ==0 and a%100!=0:
+#     print('yes')
+
+# elif a%400 ==0:
+#     print('yes')
+
+# else:
+#     print('No')
